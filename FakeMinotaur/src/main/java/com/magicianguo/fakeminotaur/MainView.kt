6a5c8f193d049dcaadb28de09831f3dca0cef03e
guojc <2639880697@qq.com>
2024-06-23 22:22:00 +0800

    简单的旋转效果

--- FakeMinotaur/src/main/java/com/magicianguo/fakeminotaur/MainView.kt
+++ FakeMinotaur/src/main/java/com/magicianguo/fakeminotaur/MainView.kt
@@ -1,9 +1,11 @@
 package com.magicianguo.fakeminotaur
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.animation.LinearInterpolator
 import android.widget.FrameLayout
 import android.widget.TextView
 
@@ -11,6 +13,16 @@ class MainView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
     private val mTvMain: TextView
+    private val mRotateAnimator = ValueAnimator().apply {
+        setFloatValues(-30F, 330F)
+        repeatCount = ValueAnimator.INFINITE
+        duration = 30 * 1000L
+        interpolator = LinearInterpolator()
+    }
+
+    private val mRotateListener = ValueAnimator.AnimatorUpdateListener { animator ->
+        onRotateUpdate(animator)
+    }
 
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_main_view, this)
@@ -21,5 +33,12 @@ class MainView @JvmOverloads constructor(
         postDelayed({
             mTvMain.text = "Sad Minotaur -25-\nConventional Tests (8)"
         }, 2000L)
+        mRotateAnimator.addUpdateListener(mRotateListener)
+        mRotateAnimator.start()
+    }
+
+    private fun onRotateUpdate(animator: ValueAnimator) {
+        val angle = animator.animatedValue as Float
+        mTvMain.rotation = angle
     }
 }
